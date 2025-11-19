5c93dce955744ee7efcb233dfb5ab0bbc4fc7dca
Khanh Do <nhu.do@student.howest.be>
2025-11-19 12:08:59 +0100

    simon fixed stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://your-github-username.github.io',
-    base: '/cck/',
+    site: 'https://nhuddos.github.io',
+    base: '/astro-deployment-demo/',
 });
